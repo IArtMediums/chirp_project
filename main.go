a81f915634fd3b2e954aeb3b8164b8cd9080c4f0
iartmediums <iartmediums@protonmail.com>
2026-02-10 13:46:30 +0100

    implemented custom handler

--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"io"
 )
 
+var port string = "8080"
+var filePathRoot string = "/app/"
+
 func main() {
-	handler := http.NewServeMux()
+	mux := http.NewServeMux()
+	mux.Handle(filePathRoot, http.StripPrefix(filePathRoot, http.FileServer(http.Dir("."))))
+	registerHandlerFunctions(mux)
 	server := http.Server{}
-	server.Handler = handler
-	server.Addr = ":8080"
+	server.Handler = mux
+	server.Addr = ":" + port
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return
 	}
+	fmt.Printf("Server is running on port %v\n", port)
+}
+
+func registerHandlerFunctions(mux *http.ServeMux) {
+	mux.HandleFunc("/healthz", HandlerHealthz)
+}
+
+func HandlerHealthz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
 }
