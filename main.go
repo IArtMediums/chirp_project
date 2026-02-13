703c077171e105f1f9d16d9cf5eb75921ef338c8
iartmediums <iartmediums@protonmail.com>
2026-02-13 13:07:12 +0100

    added config endpoints

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ import (
 	"net/http"
 	"fmt"
 	"io"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 var port string = "8080"
 var filePathRoot string = "/app/"
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle(filePathRoot, http.StripPrefix(filePathRoot, http.FileServer(http.Dir("."))))
-	registerHandlerFunctions(mux)
+	config := &apiConfig{fileserverHits: atomic.Int32{}, }
+	mux.Handle(filePathRoot, config.middlewareMetricsInc(GetFileServerHandler()))
+	registerHandlerFunctions(mux, config)
 	server := http.Server{}
 	server.Handler = mux
 	server.Addr = ":" + port
@@ -20,11 +26,12 @@ func main() {
 		fmt.Printf("%v\n", err)
 		return
 	}
-	fmt.Printf("Server is running on port %v\n", port)
 }
 
-func registerHandlerFunctions(mux *http.ServeMux) {
-	mux.HandleFunc("/healthz", HandlerHealthz)
+func registerHandlerFunctions(mux *http.ServeMux, cfg *apiConfig) {
+	mux.HandleFunc("GET /healthz", HandlerHealthz)
+	mux.HandleFunc("GET /metrics", cfg.displayMetrics())
+	mux.HandleFunc("POST /reset", cfg.reset())
 }
 
 func HandlerHealthz(w http.ResponseWriter, req *http.Request) {
@@ -32,3 +39,34 @@ func HandlerHealthz(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, "OK")
 }
+
+func GetFileServerHandler() http.Handler {
+	return http.StripPrefix(filePathRoot, http.FileServer(http.Dir(".")))
+}
+
+func (a *apiConfig) reset() func (http.ResponseWriter, *http.Request) {
+	return func (w http.ResponseWriter, req *http.Request) {
+		a.fileserverHits.Store(0)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "OK")
+	}
+}
+
+func (a *apiConfig) displayMetrics() func (http.ResponseWriter, *http.Request) {
+	return func (w http.ResponseWriter, req *http.Request) {
+		text := ""
+		text += fmt.Sprintf("Hits: %v\n", a.fileserverHits.Load())
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, text)
+	}
+}
+
+func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func (w http.ResponseWriter, req *http.Request) {
+		a.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+}
+
